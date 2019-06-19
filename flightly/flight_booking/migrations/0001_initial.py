2b021f3588cf0a0d0a3520e40835da4c9f4d5d88
David Parseen <david.parseen@gmail.com>
2019-06-19 12:39:16 +0300

    chore: Code liniting

--- flightly/flight_booking/migrations/0001_initial.py
+++ flightly/flight_booking/migrations/0001_initial.py
@@ -19,23 +19,47 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Flight',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('name', models.CharField(max_length=50, unique=True, verbose_name='Flight Name')),
-                ('departure_airport', models.CharField(max_length=50, verbose_name='Departure Airport')),
-                ('arrival_airport', models.CharField(max_length=50, verbose_name='Arrival Airport')),
-                ('departure_datetime', models.DateTimeField(verbose_name='Departure Time')),
+                ('id', models.UUIDField(default=uuid.uuid4,
+                                        editable=False, primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=50,
+                                          unique=True, verbose_name='Flight Name')),
+                ('departure_airport', models.CharField(
+                    max_length=50, verbose_name='Departure Airport')),
+                ('arrival_airport', models.CharField(
+                    max_length=50, verbose_name='Arrival Airport')),
+                ('departure_datetime', models.DateTimeField(
+                    verbose_name='Departure Time')),
                 ('capacity', models.IntegerField(verbose_name='Carrying Capacity')),
-                ('price', models.DecimalField(decimal_places=2, default=0, max_digits=10, verbose_name='Ticket Price')),
+                ('price', models.DecimalField(decimal_places=2,
+                                              default=0, max_digits=10, verbose_name='Ticket Price')),
             ],
         ),
         migrations.CreateModel(
             name='Reservation',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('ticket_number', models.CharField(default=flightly.flight_booking.models.Reservation.generate_ticket_number, max_length=23, unique=True)),
-                ('status', models.CharField(choices=[('unpaid', 'Unpaid'), ('paid', 'Paid'), ('cancelled', 'Cancelled')], default='unpaid', max_length=10)),
-                ('flight', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='flight_booking.Flight')),
-                ('traveler', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id', models.UUIDField(default=uuid.uuid4,
+                                        editable=False, primary_key=True, serialize=False)),
+                (
+                    'ticket_number',
+                    models.CharField(
+                        default=flightly.flight_booking.models.Reservation.generate_ticket_number,
+                        max_length=23,
+                        unique=True)),
+                ('status',
+                 models.CharField(choices=[('unpaid',
+                                            'Unpaid'),
+                                           ('paid',
+                                            'Paid'),
+                                           ('cancelled',
+                                            'Cancelled')],
+                                  default='unpaid',
+                                  max_length=10)),
+                ('flight',
+                 models.ForeignKey(null=True,
+                                   on_delete=django.db.models.deletion.SET_NULL,
+                                   to='flight_booking.Flight')),
+                ('traveler', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
