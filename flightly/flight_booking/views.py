005d4a0b2e1799cb63f0fccfd70ac6f5091777a5
David Parseen <david.parseen@gmail.com>
2019-06-19 13:08:17 +0300

    feat: API func for Flight Booking with no permission restrictions

--- flightly/flight_booking/views.py
+++ flightly/flight_booking/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from flightly.flight_booking.models import Flight, Reservation
+from flightly.flight_booking.serializers import FlightSerializer, ReservationSerializer
+
+
+class FlightsApiView(generics.ListCreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class FlightDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class ReservationListView(generics.ListCreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class ReservationDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
