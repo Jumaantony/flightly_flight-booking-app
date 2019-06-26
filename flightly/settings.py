581ec69577afe06e53aff621f84489bb5acabbf4
David Parseen <david.parseen@gmail.com>
2019-06-26 08:05:04 +0300

    feat: Adds default pagination to allow the app to handle a larger amount of data.

--- flightly/settings.py
+++ flightly/settings.py
@@ -126,3 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "users.FlightlyUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 30
+}
