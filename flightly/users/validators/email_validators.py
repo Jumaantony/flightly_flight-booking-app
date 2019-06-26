0644c3498c676724555c0f63ae62a81c60c3c453
David Parseen <david.parseen@gmail.com>
2019-06-26 22:12:10 +0300

    feat: Adds regex validation for password

---
+++ flightly/users/validators/email_validators.py
@@ -0,0 +1,7 @@
+"""
+Django already provides a very powerful Email Validator which is  natively used by Django Rest Framework.
+Therefore there is no need to rewrite this functionality.
+
+The Email Validator is located at django.core.validators.EmailValidator
+It conclusively covers edge cases when it comes to emails.
+"""
