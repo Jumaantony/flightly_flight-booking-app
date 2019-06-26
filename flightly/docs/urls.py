d237b1050b18580ba44d3c640caecc24e6f107fe
David Parseen <david.parseen@gmail.com>
2019-06-26 17:41:21 +0300

    feat: Adds landing page with link to docs

--- flightly/docs/urls.py
+++ flightly/docs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework.documentation import include_docs_urls
 
+from flightly.docs.views import Index
+
 urlpatterns = [
     path('docs/', include_docs_urls(title='Flightly API')),
+    path('', Index.as_view(), name='index' ),
 ]
