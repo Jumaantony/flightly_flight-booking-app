10bcd77a2571fde6e9f65516a7f557ca0017b461
David Parseen <david.parseen@gmail.com>
2019-06-26 08:07:50 +0300

    chore: Renamed create method for users so as to leverage DRF's data validation from the serializer before sending it to the Model for creation.

--- flightly/users/models.py
+++ flightly/users/models.py
@@ -19,7 +19,7 @@ class FlightlyUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
