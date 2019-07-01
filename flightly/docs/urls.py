aee135248e41051e85f1721cb84f65f5517070e4
David Parseen <david.parseen@gmail.com>
2019-07-01 17:30:51 +0300

    fix: fixing docs

--- flightly/docs/urls.py
+++ flightly/docs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework.documentation import include_docs_urls
+from rest_framework.documentation import include_docs_urls, get_schema_view
 
 from flightly.docs.views import Index
 
 urlpatterns = [
     path('docs/', include_docs_urls(title='Flightly API')),
+    path('schema/', get_schema_view(title='Flightly API')),
     path('', Index.as_view(), name='index'),
 ]
