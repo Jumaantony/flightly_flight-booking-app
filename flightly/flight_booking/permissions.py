e376de5bb897a6db0df698582469324ce3c778b9
David Parseen <david.parseen@gmail.com>
2019-07-03 14:19:57 +0300

    Setup permissions for flight_booking app

---
+++ flightly/flight_booking/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import (
+        BasePermission, IsAuthenticated,
+        IsAdminUser, SAFE_METHODS)
+
+class IsTravelerOrAdminOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return obj.traveler == user or user.is_admin or user.is_superuser
+
+class IsReservationOwnerOrAdminOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return obj.traveler == user or user.is_staff or user.is_superuser
+
+class IsAdminUserOrReadOnly(IsAdminUser):
+
+    def has_permission(self, request, view):
+        is_admin = super(
+            IsAdminUserOrReadOnly,
+            self).has_permission(request, view)
+        return request.method in SAFE_METHODS or is_admin
