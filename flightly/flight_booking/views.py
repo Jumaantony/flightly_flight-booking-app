66ecec3a8ab261ce9c11571ddfed9f0f3f4b078e
David Parseen <david.parseen@gmail.com>
2019-07-03 14:19:57 +0300

    Setup permissions for flight_booking app

--- flightly/flight_booking/views.py
+++ flightly/flight_booking/views.py
@@ -1,24 +1,46 @@
 from rest_framework import generics
+from django.db.models.query import QuerySet
+
 
 from flightly.flight_booking.models import Flight, Reservation
 from flightly.flight_booking.serializers import FlightSerializer, ReservationSerializer
+from flightly.flight_booking.permissions import (
+    IsReservationOwnerOrAdminOnly, IsAuthenticated,
+    IsAdminUserOrReadOnly
+    )
 
 
 class FlightsApiView(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUserOrReadOnly,)
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
 
 
 class FlightDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAdminUserOrReadOnly,)
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
 
 
 class ReservationListView(generics.ListCreateAPIView):
-    queryset = Reservation.objects.all()
+    permission_classes = (IsAuthenticated,)
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            queryset = Reservation.objects.all()
+        else:
+            queryset = Reservation.objects.filter(
+            traveler = user
+            )
+        if isinstance(queryset, QuerySet):
+            queryset = queryset.all()
+        return queryset
+
 
 
 class ReservationDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsReservationOwnerOrAdminOnly,)
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
