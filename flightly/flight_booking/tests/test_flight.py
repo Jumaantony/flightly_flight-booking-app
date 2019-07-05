4a7add3a44aa05a60206c17c9f9d7181a415a83a
David Parseen <david.parseen@gmail.com>
2019-07-05 16:33:09 +0300

    chore: Setup first flight test

--- flightly/flight_booking/tests/test_flight.py
+++ flightly/flight_booking/tests/test_flight.py
@@ -3,6 +3,24 @@ from flightly.helpers.db.db_populator import populateFlights
 from .commons import SetupWithNoUser, SetupWithAdminUser, SetupWithNonAdminUser
 
 class SetupFlights(SetupWithNoUser):
+
+
     def setUp(self):
-        populateFlights(5)
-        super(SetupFlights).setUp()
+        super().setUp()
+        self.list_uri = self.base_url + 'flights/'
+        self.detail_uri = self.base_url +'flight/'
+
+
+    @classmethod
+    def setUpTestData(self):
+        self.inserted_num = 5
+        self.flights = populateFlights(self.inserted_num)
+
+class TestWithAnonUser(SetupFlights):
+
+    def test_user_can_view_list_of_flights(self):
+        response = self.client.get(self.list_uri)
+        self.assertEqual(response.status_code, 200,
+                         f'Expected Response Code 200, received {response.status_code} instead.')
+        self.assertEqual(response.data['count'], self.inserted_num,
+                          f"Expected {self.inserted_num} Flights, received {response.data['count']} instead.")
